e717b5d9310ff72ef1cce4d88e12b43fe101ab9b
YuChieh-Chiu <81817517+YuChieh-Chiu@users.noreply.github.com>
2024-06-04 23:51:25 +0800

    Hash Table、String、Greedy

--- Easy/409_Longest Palindrome.py
+++ Easy/409_Longest Palindrome.py
@@ -5,14 +5,12 @@ class Solution:
         1. 出現偶數次的直接加上去
         2. 出現奇數次的加上該數字減一次，再在最後加一
         """
-
         s_cnt = {}
         for c in s:
             if c in s_cnt:
                 s_cnt[c] += 1
             else:
                 s_cnt[c] = 1
-
         length = 0
         odd_exist = False
         for k, v in s_cnt.items():
@@ -21,8 +19,6 @@ class Solution:
             else:
                 length += (v-1)
                 odd_exist = True
-
         if odd_exist:
             length += 1
-
         return length
