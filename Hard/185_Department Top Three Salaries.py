23c85dc1aede70cbaa2badf8570d6edc12d57cf2
YuChieh-Chiu <81817517+YuChieh-Chiu@users.noreply.github.com>
2024-06-22 23:44:30 +0800

    Database

---
+++ Hard/185_Department Top Three Salaries.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    """
+    thought:
+    - we need to `merge` two dataframe employee and department
+    - we should do the following steps to get the top3 salary in each department:
+        (1) `groupby` department name
+        (2) `drop duplicate` salary
+        (3) `sort` salary by `value` in descending order
+        (4) get the `top3` salary in each department
+    - `merge` back top3 dataframe with employee dataframe to get the employee name
+    - `rename` columns to get correct column names
+    """
+    employee = employee.merge(department, 
+        how="left",
+        left_on="departmentId", 
+        right_on="id", 
+        suffixes=("_emp", "_dept"))
+    top3 = employee[["name_dept", "salary"]].groupby(["name_dept"]).apply(lambda x: x.drop_duplicates().sort_values(["salary"], ascending=False)[:3])
+    top3.reset_index(inplace=True, drop=True)
+    top3 = top3.merge(employee,
+        how="left",
+        on=["name_dept", "salary"])
+    top3.rename(
+        columns={
+            "name_dept": "Department",
+            "name_emp": "Employee",
+            "salary": "Salary"
+        },
+        inplace=True
+    )
+    top3 = top3.loc[:, ["Department", "Employee", "Salary"]]
+    return top3
