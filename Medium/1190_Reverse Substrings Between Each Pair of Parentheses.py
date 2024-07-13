2bfb040e1bb46a49563736595c94b89ce443ed2c
YuChieh-Chiu <81817517+YuChieh-Chiu@users.noreply.github.com>
2024-07-14 02:12:49 +0800

    String、Stack

--- Medium/1190_Reverse Substrings Between Each Pair of Parentheses.py
+++ Medium/1190_Reverse Substrings Between Each Pair of Parentheses.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        """
+        '''
         thought:
         - IMPORTANT: the first encountered ")" is the right parenthesis of the innermost substring.
         - Therefore, we can follow these steps:
@@ -10,12 +10,12 @@ class Solution:
             (2) to reverse the substring inside the parentheses, we can use the `stack` to achieve this with the LIFO (Last In, First Out) concept: while there are characters in the stack and the top of the stack is not "(", pop characters from the stack and append them to `temp` (reversing from right to left).
             (3) after the loop in step (2) ends, pop the last character from the stack (which corresponds to the "("), and add the reversed substring to the stack, preparing for the next reversal inside the next pair of parentheses.
             (4) repeat the above steps until the end. Finally, join the characters in the list to form the final string.
-        """
+        '''
         stack = []
         for char in s:
             if char == ")":
                 temp = []
-                while stack & stack[-1] != "(":
+                while stack and stack[-1] != "(":
                     temp.append(stack.pop())
                 stack.pop()  # Remove "("
                 stack.extend(temp)
